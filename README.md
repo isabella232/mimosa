c9eb09aa9aba38ad02074817f6e892ab1bf2cd06
Scott Walker <scott.walker@puppet.com>
2019-10-09 13:11:30 +0100

    Working trigger funct

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mimosa
+
+Experiments with google cloud
+
---
+++ functions/helloworld/trigger.go
@@ -0,0 +1,37 @@
+// Package helloworld provides a set of Cloud Functions samples.
+package helloworld
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"cloud.google.com/go/functions/metadata"
+)
+
+// GCSEvent is the payload of a GCS event.
+type GCSEvent struct {
+	Bucket         string    `json:"bucket"`
+	Name           string    `json:"name"`
+	Metageneration string    `json:"metageneration"`
+	ResourceState  string    `json:"resourceState"`
+	TimeCreated    time.Time `json:"timeCreated"`
+	Updated        time.Time `json:"updated"`
+}
+
+// HelloGCSInfo prints information about a GCS event.
+func HelloGCSInfo(ctx context.Context, e GCSEvent) error {
+	meta, err := metadata.FromContext(ctx)
+	if err != nil {
+		return fmt.Errorf("metadata.FromContext: %v", err)
+	}
+	log.Printf("Event ID: %v\n", meta.EventID)
+	log.Printf("Event type: %v\n", meta.EventType)
+	log.Printf("Bucket: %v\n", e.Bucket)
+	log.Printf("File: %v\n", e.Name)
+	log.Printf("Metageneration: %v\n", e.Metageneration)
+	log.Printf("Created: %v\n", e.TimeCreated)
+	log.Printf("Updated: %v\n", e.Updated)
+	return nil
+}

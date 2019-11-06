2a59cda690c7b6c6f1dcb3ad69f1cb315f1d4505
Scott Walker <scott.walker@puppet.com>
2019-11-06 15:49:52 +0000

    Review comments

--- ui/mimosa-ui/src/components/HostDataTable.jsx
+++ ui/mimosa-ui/src/components/HostDataTable.jsx
@@ -41,19 +41,17 @@ class HostDataTable extends React.Component {
   callCloudFunction = (functionName, data) => {
 
     firebase.auth().currentUser.getIdToken().then(function (idToken) {
-      // ACCESS TOKEN SHOULD VBE ADDED AS A BEARER TOKEN
+      // FIXME - ACCESS TOKEN SHOULD BE ADDED AS A BEARER TOKEN
       fetch('https://mimosa-esp-tfmdd2vwoq-uc.a.run.app/' + functionName + "?access_token=" + idToken, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: 'same-origin', // include, *same-origin, omit
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
         headers: {
           'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: 'follow', // manual, *follow, error
-        referrer: 'no-referrer', // no-referrer, *client
-        body: data// body data type must match "Content-Type" header
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: data
       }).then(response => {
         console.log(response.status)
         console.log(response.text())
@@ -67,6 +65,7 @@ class HostDataTable extends React.Component {
     });
 
   }
+
   componentDidMount() {
     //fakeData to be used for styling, visual fixes, rather than hitting DB
     // let fakeData = [

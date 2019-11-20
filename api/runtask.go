a287486130e984a4ddf02984d68488b0bdf945ac
Scott Walker <scott.walker@puppet.com>
2019-11-20 15:55:59 +0000

    Run task API call changes

--- api/runtask.go
+++ api/runtask.go
@@ -23,21 +23,26 @@ func RunTask(w http.ResponseWriter, r *http.Request) {
 	untrustedData, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "failed to read body", http.StatusInternalServerError)
+		return
 	}
 	r.Body.Close()
 	var target target
 	err = json.Unmarshal(untrustedData, &target)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("failed to unmarshal body: %v", err), http.StatusBadRequest)
+		return
 	}
+	log.Printf("target: %v", target)
 
 	// Validate
 	// FIXME we need to check this user has permission to run this task
 	if target.Workspace == "" {
 		http.Error(w, "workspace must be specified", http.StatusBadRequest)
+		return
 	}
 	if target.ID == "" {
 		http.Error(w, "id must be specified", http.StatusBadRequest)
+		return
 	}
 
 	// We don't trust the data posted to us enough to repost the same bytes to Reusabolt
@@ -45,23 +50,27 @@ func RunTask(w http.ResponseWriter, r *http.Request) {
 	trustedData, err := json.Marshal(target)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("failed to marshal message: %v", err), http.StatusInternalServerError)
+		return
 	}
 
 	// Send the pubsub message to Reusabolt
 	ctx := context.Background()
 	project := os.Getenv("GCP_PROJECT")
 	if len(project) == 0 {
-		log.Fatal("GCP_PROJECT environment variable must be set")
+		http.Error(w, "GCP_PROJECT environment variable must be set", http.StatusInternalServerError)
+		return
 	}
 	client, err := pubsub.NewClient(ctx, project)
 	if err != nil {
-		log.Fatalf("failed to create pubsub client: %v", err)
+		http.Error(w, fmt.Sprintf("failed to create pubsub client: %v", err), http.StatusInternalServerError)
+		return
 	}
 	topic := client.TopicInProject("reusabolt", project)
 	result := topic.Publish(ctx, &pubsub.Message{Data: trustedData})
 	_, err = result.Get(ctx)
 	if err != nil {
-		log.Fatalf("failed to publish a message to the 'reusabolt' topic: %v", err)
+		http.Error(w, fmt.Sprintf("failed to publish a message to the 'reusabolt' topic: %v", err), http.StatusInternalServerError)
+		return
 	}
 
 }
--- ui/mimosa-ui/src/view/RunTask.jsx
+++ ui/mimosa-ui/src/view/RunTask.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import {NavMenu} from '../components';
+import { NavMenu } from '../components';
 import { withFirebase } from '../utils/Firebase';
 import { withRouter } from 'react-router-dom';
-import {Container, Divider, Header, Form, Button, Message, Dropdown} from 'semantic-ui-react';
+import { Container, Divider, Header, Form, Button, Message, Dropdown } from 'semantic-ui-react';
 
 class RunTask extends Component {
   // Call cloud function, since we don't expect result we don't do anything
   callCloudFunction = (functionName, hostid) => {
     if (this.props.firebase.auth.currentUser) {
-      var wsid = this.props.match.params;
+      var wsid = this.props.match.params.wsid;
       this.props.firebase.auth.currentUser.getIdToken().then(function (idToken) {
         // FIXME - ACCESS TOKEN SHOULD BE ADDED AS A BEARER TOKEN
         fetch('https://mimosa-esp-tfmdd2vwoq-uc.a.run.app/' + functionName + "?access_token=" + idToken, {
@@ -39,7 +39,7 @@ class RunTask extends Component {
     const { authUser } = this.props;
     const { wsid } = this.props.match.params;
     var hasHosts, docId;
-    if(this.props.location.state && this.props.location.state.response.length > 0) {
+    if (this.props.location.state && this.props.location.state.response.length > 0) {
       hasHosts = this.props.location.state.response
       docId = this.props.location.state.doc
     } else {
@@ -48,7 +48,7 @@ class RunTask extends Component {
     console.log(hasHosts);
     console.log('From data state ', hasHosts);
     const option = [
-      {text: hasHosts, value: hasHosts}
+      { text: hasHosts, value: hasHosts }
     ]
     return (
       <div>
@@ -59,11 +59,11 @@ class RunTask extends Component {
           <Form>
             <Form.Field>
               <label>Task Name</label>
-              <input value="facts"/>
+              <input value="facts" />
             </Form.Field>
             <Form.Field disabled>
               <label>Params</label>
-              <input placeholder="e.g. verbose"/>
+              <input placeholder="e.g. verbose" />
             </Form.Field>
             <Form.Field disabled>
               <label>Note</label>
@@ -77,7 +77,7 @@ class RunTask extends Component {
                   defaultValue={option[0].value}
                 />
                 <Divider />
-                <Button onClick={this.callCloudFunction('facts', docId)} type="submit">Run</Button>
+                <Button onClick={this.callCloudFunction('api/v1/runtask', docId)} type="submit">Run</Button>
               </div>
               :
               <div>

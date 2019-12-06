3edc041a834038af419ca088a18932acbe8db288
Philip-Smyth <philip.smyth@philips-mbp.eth.bfs.corp.puppet.net>
2019-12-06 09:34:27 +0000

    Delete host data from firestore when delete event type is sent

--- worldbuilders/inventory/inventory.go
+++ worldbuilders/inventory/inventory.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"time"
 
 	"cloud.google.com/go/firestore"
@@ -44,59 +45,12 @@ func build(convert conversionFunc) pubsubHandlerFunc {
 
 		// Unmarshal the message
 		var routerMessage routerMessage
+
 		err := json.Unmarshal(m.Data, &routerMessage)
 		if err != nil {
 			return fmt.Errorf("failed to unmarshal router message: %v", err)
 		}
 		log.Printf("router message: %v", routerMessage)
-
-		// FIXME Check version is supported
-		if routerMessage.MimosaTypeVersion == "" {
-			return fmt.Errorf("no version found in the router message: %v", err)
-		}
-
-		// Read object from the bucket
-		client, err := storage.NewClient(ctx)
-		if err != nil {
-			return err
-		}
-		obj := client.Bucket(routerMessage.Bucket).Object(routerMessage.Name)
-		attrs, err := obj.Attrs(ctx)
-		if err != nil {
-			return err
-		}
-
-		//NOTE: we could cross-check these values vs the router message (e.g. routerMessage.Version), or just use one mechanism
-		if _, ok := attrs.Metadata["mimosa-type"]; !ok {
-			return fmt.Errorf("missing metadata mimosa-type")
-		}
-		if _, ok := attrs.Metadata["mimosa-type-version"]; !ok {
-			return fmt.Errorf("missing metadata mimosa-type-version")
-		}
-		rc, err := obj.NewReader(ctx)
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-		object, err := ioutil.ReadAll(rc)
-		if err != nil {
-			return err
-		}
-
-		// Convert the object to a host
-		// FIXME pass the mimosa-type and mimosa-type-version value to the convert func
-		host, err := convert(object)
-		if err != nil {
-			return err
-		}
-		if host.Name == "" {
-			return fmt.Errorf("host must have a name: %v", host)
-		}
-
-		// Update host fields
-		source := routerMessage.Bucket
-		host.Source = source
-		host.Timestamp = time.Now().Format(time.RFC3339)
 
 		// Compute a deterministic hash to use as firestore ID
 		id, err := generateDeterministicID(routerMessage.Bucket, routerMessage.Name)
@@ -104,19 +58,90 @@ func build(convert conversionFunc) pubsubHandlerFunc {
 			return err
 		}
 
-		// Write the doc to the "hosts" collection
-		fc, err := firestore.NewClient(ctx, firestore.DetectProjectID)
-		if err != nil {
-			return err
+		project := os.Getenv("MIMOSA_GCP_PROJECT")
+		if len(project) == 0 {
+			project = firestore.DetectProjectID
 		}
-		_, err = fc.Collection("ws").Doc(routerMessage.Workspace).Collection("hosts").Doc(id).Set(context.Background(), host)
+		fc, err := firestore.NewClient(ctx, project)
 		if err != nil {
 			return err
 		}
 
+		switch routerMessage.EventType {
+		case "OBJECT_DELETE":
+			err = delete(ctx, fc, id, routerMessage)
+		case "OBJECT_FINALIZE":
+			err = update(ctx, fc, convert, id, routerMessage)
+		default:
+			err = fmt.Errorf("unknown event type: %s", routerMessage.EventType)
+		}
+		return err
+	}
+}
+
+func update(ctx context.Context, fc *firestore.Client, convert conversionFunc, id string, routerMessage routerMessage) error {
+	// FIXME Check version is supported
+	if routerMessage.MimosaTypeVersion == "" {
+		return fmt.Errorf("no mimosa version found in the router message")
+	}
+
+	// Read object from the bucket
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	obj := client.Bucket(routerMessage.Bucket).Object(routerMessage.Name)
+
+	// NOTE: We need to tweak how routerMessage and metadata is checked
+	// attrs, err := obj.Attrs(ctx)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// NOTE: we could cross-check these values vs the router message (e.g. routerMessage.Version), or just use one mechanism
+	// if _, ok := attrs.Metadata["mimosa-type"]; !ok {
+	// 	return fmt.Errorf("missing metadata mimosa-type")
+	// }
+	// if _, ok := attrs.Metadata["mimosa-type-version"]; !ok {
+	// 	return fmt.Errorf("missing metadata mimosa-type-version")
+	// }
+
+	rc, err := obj.NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+	object, err := ioutil.ReadAll(rc)
+	if err != nil {
 		return err
 	}
 
+	// // Convert the object to a host
+	// // FIXME pass the mimosa-type and mimosa-type-version value to the convert func
+	host, err := convert(object)
+	if err != nil {
+		return err
+	}
+	if host.Name == "" {
+		return fmt.Errorf("host must have a name: %v", host)
+	}
+	host.Timestamp = time.Now().Format(time.RFC3339)
+	// Write the doc to the "hosts" collection
+	_, err = fc.Collection("ws").Doc(routerMessage.Workspace).Collection("hosts").Doc(id).Set(ctx, host)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func delete(ctx context.Context, fc *firestore.Client, id string, routerMessage routerMessage) error {
+	// Write the doc to the "hosts" collection
+
+	_, err := fc.Collection("ws").Doc(routerMessage.Workspace).Collection("hosts").Doc(id).Delete(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func generateDeterministicID(bucketName, objectName string) (string, error) {
